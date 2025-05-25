621e0f3332d6f9b82dca9e187196a725df415758
aditya <ayuadi2845@gmail.com>
2025-05-25 21:14:43 +0530

    same typo

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Footwear-website",
+  base: "/footwear-website",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
