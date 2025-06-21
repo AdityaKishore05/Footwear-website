2b2cad78cf87e35320e9b7b8bf4dacdc17463f0e
aditya <ayuadi2845@gmail.com>
2025-06-21 15:17:21 +0530

    header hide on scrolling

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,11 +1,28 @@
 // Nav.jsx
+import React, { useEffect, useState } from "react";
 import "./nav.css";
-import React from 'react';
-
 
 const Nav = ({ query, handleInputChange }) => {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setShow(false); // Hide on scroll down
+      } else {
+        setShow(true); // Show on scroll up
+      }
+      setLastScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
   return (
-    <nav>
+    <nav className={`nav ${show ? "show" : "hide"}`}>
       <div className="nav-container">
         <input
           type="text"
@@ -16,7 +33,7 @@ const Nav = ({ query, handleInputChange }) => {
         />
       </div>
       <div>
-        <h1 className=" title font-matangi">Sliders</h1>
+        <h1 className="title font-matangi">Sliders</h1>
       </div>
     </nav>
   );
